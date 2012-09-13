6d1188953904144669cecd8c76be8b48b168c05b
mt-sagarmatha <eminisrafil@yahoo.com>
2012-09-13 00:49:50 -0400

    big change

--- github.php
+++ github.php
@@ -1,11 +1,13 @@
 <?php `git pull origin master`;
 	`mkdir fuck`;
+        $dir = shell_exec (" mkdir fuck2");
+	$pull = shell_exec (" git pull origin master");
 	$date = date("D M j G:i:s T Y");
 	echo "hello\n";
 	echo $date;
 	$File = "YourFile.txt";
 	$Handle = fopen($File, 'w');
-	$Data = "Jane Doe helllo it finally works\n" . $date;
+	$Data = "Jane Doe helllo it finally works\n" . $date . "\n mk dir fuck2: ".$dir . "\npull " .$pull;
 	fwrite($Handle, $Data);
 	`git pull origin master`;
-	'git pull'
+	`git pull`;
