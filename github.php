177c515f247b44518aef57324c3eca8fabdf2a12
mt-sagarmatha <eminisrafil@yahoo.com>
2012-09-13 20:00:12 +0000

    hi

--- github.php
+++ github.php
@@ -2,12 +2,14 @@
 	`mkdir fuck`;
         $dir = shell_exec (" mkdir fuck2");
 	$pull = shell_exec (" git -v pull origin master");
+	$pull2 = `git pull 2>&1`
 	$date = date("D M j G:i:s T Y");
 	echo "hello\n";
 	echo $date;
+	echo (getcwd());
 	$File = "YourFile.txt";
 	$Handle = fopen($File, 'w');
-	$Data = "Jane Doe helllo it finally works\n" . $date . "\n mk dir fuck2: ".$dir . "\npull " .$pull;
+	$Data = "Jane Doe helllo it finally works\n" . $date . "\n mk dir fuck2: ".$dir . "\npull " .$pull "\npull2" . $pull2;
 	fwrite($Handle, $Data);
 	`git pull origin master`;
 	`git commit -am 'fucker'`;
