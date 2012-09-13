a49559b40d654b6de7c41681d88abd88ef1e2158
www-data <www-data@ip-10-195-226-196.ec2.internal>
2012-09-13 20:40:27 +0000

    fucker

--- github.php
+++ github.php
@@ -1,15 +1,13 @@
 <?php `git pull origin master`;
-	`mkdir fuck`;
-        $dir = shell_exec (" mkdir fuck2");
 	$pull = shell_exec (" git -v pull origin master");
 	$pull2 = `git pull 2>&1`;
+	$who = `whoami >>&`;
 	$date = date("D M j G:i:s T Y");
 	echo "hello\n";
 	echo $date;
-	echo (getcwd());
-	$File = "YourFile.txt";
+	$File = "f.txt";
 	$Handle = fopen($File, 'w');
-	$Data = "Jane Doe helllo it finally works\n" . $date . "\n mk dir fuck2: ".$dir . "\npull " .$pull .  "\npull2" . $pull2;
+	$Data =  $date  . "\npull " .$pull .  "\npull2: " . $pull2 . "\nWho: " . $who ;
 	fwrite($Handle, $Data);
 	`git pull origin master`;
 	`git commit -am 'fucker'`;
