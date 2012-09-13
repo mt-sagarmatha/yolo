616c1e792aff77538b16c909f643bc73b8d370ed
mt-sagarmatha <eminisrafil@yahoo.com>
2012-09-13 04:22:45 +0000

    3

--- github.php
+++ github.php
@@ -1,5 +1,8 @@
 <?php `git pull origin master`;
+	$today = date("D M j G:i:s T Y");
+	echo 'hi';
+	echo $date;
 	$File = "YourFile.txt";
 	$Handle = fopen($File, 'w');
-	$Data = "Jane Doe helllo it finally works\n";
+	$Data = "Jane Doe helllo it finally works\n" . $date;
 	fwrite($Handle, $Data);
