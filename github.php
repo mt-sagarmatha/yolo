dccebc0cf1929e755503ee6346e6836c128d29c8
mt-sagarmatha <eminisrafil@yahoo.com>
2012-09-12 23:35:13 -0400

    works?!

--- github.php
+++ github.php
@@ -1,5 +1,5 @@
 <?php `git pull origin master`;
 	$File = "YourFile.txt";
 	$Handle = fopen($File, 'w');
-	$Data = "Jane Doe helllo\n";
+	$Data = "Jane Doe helllo it finally works\n";
 	fwrite($Handle, $Data);
